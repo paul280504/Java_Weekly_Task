202e53475d5d25cd7cf6e7fb16182ddb9607a71f
Paul Rajan <paulrjan124@gmail.com>
2026-02-18 09:48:29 +0530

    Day-1 Updated Task

--- src/main/java/com/kovanlabsintern/javaweektask/day1String/IdentityHashCode.java
+++ src/main/java/com/kovanlabsintern/javaweektask/day1String/IdentityHashCode.java
@@ -1,23 +1,18 @@
 package com.kovanlabsintern.javaweektask.day1String;
 
+
 public class IdentityHashCode {
-    public static void ref(String st,String st1)
+    public static String ref(String st,String st1)
     {
-        if(st==st1)
-        {
-            System.out.println("true\n"+System.identityHashCode(st)+"=="+System.identityHashCode(st1));
-        }
-        else
-        {
-            System.out.println("false\n"+System.identityHashCode(st)+"!="+System.identityHashCode(st1));
-        }
+        String result = st==st1?"true\n"+System.identityHashCode(st)+"=="+System.identityHashCode(st1):"false\n"+System.identityHashCode(st)+"!="+System.identityHashCode(st1);
+        return result;
     }
 
     public static void main(String[] args) {
         String s1 = "Hello";
         String s2 = "Hello";
         String s3 = new String("Hello");
-        ref(s1,s2);
-        ref(s1,s3);
+        System.out.println(ref(s1,s2));
+        System.out.println(ref(s1,s3));
     }
 }
